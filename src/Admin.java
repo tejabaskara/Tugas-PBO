bf380421c34cfa22e8f4d754594ffc6699728af8
tejabaskara <tejasidang@gmail.com>
2023-04-12 14:13:32 +0800

    menambahkan methode pada bagian kelas customer dan menambahkan pada kelas admin bagian tambah restauran

--- src/Admin.java
+++ src/Admin.java
@@ -2,9 +2,11 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 public class Admin {
-    private static ArrayList<String> alamat = new ArrayList<String>();
-    private static ArrayList<ArrayList<String>> restaurant = new ArrayList< ArrayList <String>>();
-    public static void MenuAdmin(){
+    public static ArrayList<String> alamat = new ArrayList<String>();
+    public static ArrayList<ArrayList<String>> restaurant = new ArrayList< ArrayList <String>>();
+    public static ArrayList<ArrayList<Integer>> hargaMenu = new ArrayList< ArrayList <Integer>>();
+    private static int banyakRestauran=0;
+    public static void MenuAdmin(){ // MENU UTAMA DALAM ADMIN SETELAH LOGIN
         System.out.println("MENU ADMIN");
         System.out.println("1. Lihat restaurant");
         System.out.println("2. Tambah restaurant");
@@ -18,10 +20,11 @@ public class Admin {
                 System.out.println("LIHAT RESTAURANT");
                 System.out.println("Daftar Restaurant");
                 lihatRestauran();
-                MenuAdmin();
+                menuRestaurant();
+//                MenuAdmin();
                 break;
             case "2":
-                TambahMenu();
+                TambahRestaurant();
                 MenuAdmin();
                 break;
             case "3":
@@ -37,38 +40,78 @@ public class Admin {
         }
     }
 
-    private static void TambahMenu(){
-        System.out.println("TAMBAH MENU");
+    private static void TambahRestaurant(){ // TEMPAT MENAMBAHKAN MENU DENGAN INPUT STRING
+
+        restaurant.add(new ArrayList<>());
+        hargaMenu.add(new ArrayList<>());
+
+        System.out.println("TAMBAH RESTAURANT");
         System.out.print("Masukkan nama restaurant: ");
         String tempNama = Utility.scanSTR();
-        restaurant.add(new ArrayList<>());
-        restaurant.get(0).add(0, tempNama);
+        restaurant.get(banyakRestauran).add(0, tempNama);
+
 
         System.out.print("Masukkan alamat restaurant: ");
         String tempAlamat = Utility.scanSTR();
         alamat.add(tempAlamat);
 
-        System.out.print("Masukkan banyak menu di restaurant: ");
-        int banyakMenu = Utility.scanINT();
-        for (int i = 1; i <= banyakMenu; i++){
+//        System.out.print("Masukkan banyak menu di restaurant: ");
+//        int banyakMenu = Utility.scanINT();
+        int pilihan = 0;
+        do {
+            int i = 1;
+//            System.out.print("masukkan menu: ");
 //            String menu = Utility.scanSTR();
             String menu = "a";
-            restaurant.get(0).add(i, menu);
-        }
+            restaurant.get(banyakRestauran).add(i, menu);
+//            System.out.print("masukkan harga: ");
+//            int harga = Utility.scanINT();
+            int harga = 2000;
+            hargaMenu.get(banyakRestauran).add(i-1, harga);
+            System.out.println("1. Tambah menu");
+            System.out.println("0. Keluar");
+            System.out.print("Masukkan pilihan: ");
+            pilihan = Utility.scanINT();
+            i+=1;
+        }while (pilihan == 1);
 
-        System.out.println(restaurant);
-        System.out.println(alamat);
+//        for (int i = 1; i <= banyakMenu; i++){
+//        }
+
+//        System.out.println(restaurant);
+//        System.out.println(alamat);
+//        System.out.println(hargaMenu);
         System.out.println(restaurant.size());
-
+        System.out.println(restaurant.get(banyakRestauran).size());
+        banyakRestauran += 1;
     }
 
-    private static void lihatRestauran(){
+    public static void menuRestaurant(){ // UNTUK MELIHAT MENU DI DALAM RESTAURANT DALAM BAGIAN LIHAT RESTAURAN
+        System.out.println("0. Keluar");
+        System.out.print("Pilih restauran untuk melihat lebih detail: ");
+        int pilihan = Utility.scanINT();
+        if (pilihan == 0){
+            MenuAdmin();
+        } else {
+            pilihan -= 1;
+            System.out.println("Nama Restaurant: " + restaurant.get(pilihan).get(0));
+            System.out.println("Alamat: " + alamat.get(pilihan));
+//            System.out.println("a");
+//            System.out.println(pilihan);
+            System.out.printf("MENU:\t\t\tHARGA:\n");
+            for (int i=1; i < restaurant.get(pilihan).size(); i++){
+                System.out.printf("%d. %s\t\t\t%s\n",i, restaurant.get(pilihan).get(i), hargaMenu.get(pilihan).get(i-1));
+            }
+        }
+    }
+
+    private static void lihatRestauran(){ // UNTUK MELIHAT RESTAURAN YANG ADA
         for (int i= 0; i < restaurant.size(); i++ ){
             System.out.printf("%d. %s\n",i+1 , restaurant.get(i).get(0));
         }
     }
 
-    public static void HapusRestauran(){
+    public static void HapusRestauran(){ // UNTUK MENGHAPUS RESTURAN BESERTA MENU MENU YANG ADA
         System.out.println("HAPUS RESTAURANT");
         System.out.println("Daftar Restaurant");
         lihatRestauran();
--- src/Customer.java
+++ src/Customer.java
@@ -1,6 +1,9 @@
-
+import java.util.ArrayList;
 
 public class Customer {
+    public static ArrayList<ArrayList<String>> restaurant = Admin.restaurant;
+    public static ArrayList<ArrayList<Integer>> hargaMenu = Admin.hargaMenu;
+    public static ArrayList<String> alamat = Admin.alamat;
 
     public static void menuCustomer(){
 
@@ -13,7 +16,7 @@ public class Customer {
 //        System.out.println(pilihan);
         switch (pilihan){
             case "1":
-                System.out.println("Buat pesanan");
+                pesanan();
                 break;
             case "2":
                 System.out.println("Lihat pesanan");
@@ -29,4 +32,64 @@ public class Customer {
 
 
     }
+
+    //BUAT PESANAN
+//    private static void TampilanRestauran(){
+//
+//    }
+//
+    private static void pesanan(){
+//        TampilanRestauran();
+        int menuPesan = 0;
+        int totalBayar = 0;
+
+
+        for (int i= 0; i < restaurant.size(); i++ ){
+            System.out.printf("%d. %s\n",i+1 , restaurant.get(i).get(0));
+        }
+        System.out.println("0. Keluar");
+        System.out.print("Pilih restauran untuk melihat lebih detail: ");
+        int pilihan = Utility.scanINT();
+        if (pilihan == 0){
+            menuCustomer();
+        } else {
+            pilihan -= 1;
+            System.out.println("Nama Restaurant: " + restaurant.get(pilihan).get(0));
+            System.out.println("Alamat: " + alamat.get(pilihan));
+//            System.out.println("a");
+//            System.out.println(pilihan);
+            do {
+                System.out.printf("MENU:\t\t\tHARGA:\n");
+                for (int i=1; i < restaurant.get(pilihan).size(); i++){
+                    System.out.printf("%d. %s\t\t\t%s\n",i, restaurant.get(pilihan).get(i), hargaMenu.get(pilihan).get(i-1));
+                }
+                System.out.println("0. Keluar");
+                System.out.print("Masukkan pilihan menu yang akan dipesan: ");
+                menuPesan = Utility.scanINT();
+                if (menuPesan == 0){
+                    System.out.println("Total harga: "+ totalBayar);
+                    break;
+                }else {
+                    System.out.println("Menu: " + restaurant.get(pilihan).get(menuPesan));
+                    int menu = menuPesan - 1 ;
+                    System.out.println("Harga: " + hargaMenu.get(pilihan).get(menu)); //untuk index dalam mencari harga dari menu
+                    System.out.print("Masukkan banyak menu yang akan dipesan: ");
+                    int banyak = Utility.scanINT();
+                    int subtotal = banyak * hargaMenu.get(pilihan).get(menu);
+                    System.out.println("Harga: " + subtotal);
+                    totalBayar += subtotal;
+                    System.out.println(menuPesan);
+                }
+            }while (true);
+        }
+
+    }
+
+    //LIHAT PESANAN YANG UDAH DILAKUKAN
+
+
+
+
+
+
 }
